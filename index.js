65856ac183126e589b3f44f8c327b18b3edd9a72
DevWithEasy <devwitheasy@gmail.com>
2023-12-07 01:22:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const divisions = require('./assets/divisions.json')
+const districts = require('./assets/districts.json')
+const upazillas = require('./assets/upazillas.json')
+const postOffices = require('./assets/postOffice.json')
+const unions = require('./assets/unions.json')
+
+class BD {
+    static divisions(){
+        return divisions.divisions
+    }
+
+    static districts(){
+        return districts.districts
+    }
+
+    static district(id){
+        if(!id){
+            return {message : 'Plese insert a division id as params.'}
+        }
+        return this.districts()
+        .filter(district=> district.division_id === id.toString())
+    }
+
+    static upazillas(){
+        return upazillas.upazillas
+    }
+
+    static upazilla(id){
+        if(!id){
+            return {message : 'Plese insert a district id as params.'}
+        }
+        return this.upazillas()
+        .filter(upazilla=> upazilla.district_id === id.toString())
+    }
+
+    static unions(){
+        return unions.unions
+    }
+
+    static union(id){
+        if(!id){
+            return {message : 'Plese insert a upazilla id as params.'}
+        }
+        return this.unions()
+        .filter(union=> union.upazilla_id === id.toString())
+    }
+
+    static postOffices(){
+        return postOffices
+    }
+
+    static postOffice(id){
+        if(!id){
+            return {message : 'Plese insert a district id as params.'}
+        }
+
+        const district = this.districts().find(district=> district.id === id.toString())
+
+        if(!district){
+            return {message : 'Plese insert a district id as params.'}
+        }
+
+        const find = []
+
+        const data = this.postOffices()
+
+        for (const key in data){
+            const entry = data[key]
+            
+            if(entry.bn && entry.bn.district.trim() === district.bn_name.trim()){
+                find.push(entry.bn)
+            }
+        }
+
+        return find
+    }
+
+}
